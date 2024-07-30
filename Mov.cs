74d8c5e179a5ca43acd9dd98539cc994d5d97343
Luciano <lu.beneventi@duocuc.cl>
2024-07-29 20:56:51 -0400

    Add files via upload

--- Mov.cs
+++ Mov.cs
@@ -4,48 +4,56 @@ using UnityEngine;
 
 public class Mov : MonoBehaviour
 {
+    public GameObject hitMarker;
+
     
-    [DllImport("user32.dll", CharSet = CharSet.Auto)]
-    private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
-
-    [DllImport("user32.dll", CharSet = CharSet.Auto)]
-    private static extern bool PostMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
-
-    private const uint WM_USER = 0x0400;
-    private const uint WM_HITMARKERMOVE = WM_USER + 1;
-
-    private IntPtr targetWindow;
-
     void Start()
     {
         
-        targetWindow = FindWindow(null, "TargetApplicationWindowName");
-
-        if (targetWindow == IntPtr.Zero)
+        if (hitMarker == null)
         {
-            Debug.LogError("Target window not found");
+            Debug.LogError("HitMarker GameObject is not assigned.");
         }
     }
 
+   
     void Update()
     {
         
-        Vector3 position = transform.position;
+
+    }
+
+    void OnMessage(string message)
+    {
+        
+        var data = JsonUtility.FromJson<MessageData>(message);
 
         
-        int x = Mathf.FloorToInt(position.x * 100);
-        int y = Mathf.FloorToInt(position.y * 100);
-        int z = Mathf.FloorToInt(position.z * 100);
-
-        
-        int packedPosition = (x & 0xFFFF) | ((y & 0xFFFF) << 16);
-
-        
-        if (targetWindow != IntPtr.Zero)
+        if (data.type == "cursor")
         {
-            PostMessage(targetWindow, WM_HITMARKERMOVE, (IntPtr)packedPosition, (IntPtr)z);
+            
+            UpdateHitMarkerPosition(data.x, data.y);
         }
     }
+
+    void UpdateHitMarkerPosition(float x, float y)
+    {
+       
+        Vector3 screenPosition = new Vector3(x, y, Camera.main.nearClipPlane);
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
+
+        
+        hitMarker.transform.position = worldPosition;
+    }
+}
+
+
+[System.Serializable]
+public class MessageData
+{
+    public string type;
+    public float x;
+    public float y;
 }
 
 
